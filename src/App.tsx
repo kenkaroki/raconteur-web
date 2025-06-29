33cc132db72749276811764414f3e9687b1d4c15
kenkaroki <kenkaroki92@gmail.com>
2025-06-29 18:53:04 +0300

    updated app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/home";
 import Books from "./components/books";
 import Admin from "./components/admin";
